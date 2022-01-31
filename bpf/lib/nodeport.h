8294e5967a37e3e7814999b407b1b14d87e64f7e
Paul Chaignon <paul@cilium.io>
2022-01-31 22:00:56 +0100

    bpf: Implement IPv6 BPF masquerading
    This commit implements BPF masquerading for IPv6 by following the
    implementation for IPv4. The main function changed here is
    snat_v6_needed with almost the exact same steps as IPv4.
    
    Support for the ipmasq agent in IPv6 is not implemented here.
    
    [ Quentin: Rebased on master branch, addressed conflicts in
      nodeport.h due to commits:
      - 0ea434115253 ("bpf: Make union v6addr const where possible")
      - c62b2094dbec ("bpf: nat: move snat_v{4,6}_needed into lib/nat.h")
      - b3747c8a1bf0 ("bpf: Change ipcache lookup functions to take ClusterID")
      - 9f6fc7e16df9 ("bpf: nodeport: fix tracing for handle_nat_fwd()")
      - 54a86312782f ("bpf: nodeport: handle revDNAT for local backends at to-netdev/to-overlay")
      - eee902f44979 ("bpf: conntrack: introduce an optimized CT lookup for LB")
      - 05d084baeef1 ("bpf: nodeport: move DSR handling from bpf_lxc into from/to-netdev") ]
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>
    Signed-off-by: Quentin Monnet <quentin@isovalent.com>

--- bpf/lib/nodeport.h
+++ bpf/lib/nodeport.h
@@ -97,8 +97,7 @@ static __always_inline bool nodeport_uses_dsr6(const struct ipv6_ct_tuple *tuple
 	return nodeport_uses_dsr(tuple->nexthdr);
 }
 
-static __always_inline int nodeport_snat_fwd_ipv6(struct __ctx_buff *ctx,
-						  const union v6addr *addr)
+static __always_inline int nodeport_snat_fwd_ipv6(struct __ctx_buff *ctx)
 {
 	struct ipv6_nat_target target = {
 		.min_port = NODEPORT_PORT_MIN_NAT,
@@ -106,9 +105,7 @@ static __always_inline int nodeport_snat_fwd_ipv6(struct __ctx_buff *ctx,
 	};
 	int ret;
 
-	ipv6_addr_copy(&target.addr, addr);
-
-	ret = snat_v6_needed(ctx, addr) ?
+	ret = snat_v6_needed(ctx, &target.addr) ?
 	      snat_v6_nat(ctx, &target) : CTX_ACT_OK;
 	if (ret == NAT_PUNT_TO_STACK)
 		ret = CTX_ACT_OK;
@@ -888,7 +885,9 @@ skip_service_lookup:
 			if (IS_ERR(ret))
 				return ret;
 
+#ifndef ENABLE_MASQUERADE
 			return CTX_ACT_OK;
+#endif
 		}
 #endif /* ENABLE_DSR */
 
@@ -1172,17 +1171,12 @@ int tail_handle_snat_fwd_ipv6(struct __ctx_buff *ctx)
 	enum trace_point obs_point;
 	int ret;
 #if defined(TUNNEL_MODE) && defined(IS_BPF_OVERLAY)
-	union v6addr addr = { .p1 = 0 };
-
 	obs_point = TRACE_TO_OVERLAY;
-	BPF_V6(addr, ROUTER_IP);
 #else
-	union v6addr addr = IPV6_DIRECT_ROUTING;
-
 	obs_point = TRACE_TO_NETWORK;
 #endif
 
-	ret = nodeport_snat_fwd_ipv6(ctx, &addr);
+	ret = nodeport_snat_fwd_ipv6(ctx);
 	if (IS_ERR(ret))
 		return send_drop_notify_error(ctx, 0, ret, CTX_ACT_DROP, METRIC_EGRESS);
 
@@ -1201,7 +1195,8 @@ __handle_nat_fwd_ipv6(struct __ctx_buff *ctx, struct trace_ctx *trace)
 		return ret;
 
 #if !defined(ENABLE_DSR) ||						\
-    (defined(ENABLE_DSR) && defined(ENABLE_DSR_HYBRID))
+    (defined(ENABLE_DSR) && defined(ENABLE_DSR_HYBRID)) ||		\
+     defined(ENABLE_MASQUERADE)
 	if (!ctx_snat_done(ctx)) {
 		ep_tail_call(ctx, CILIUM_CALL_IPV6_NODEPORT_SNAT_FWD);
 		ret = DROP_MISSED_TAIL_CALL;
